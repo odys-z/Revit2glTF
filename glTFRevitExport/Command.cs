8f6c777edc15a4225aa8b8a4cb9f02e41abafca4
Odys Zhou <odys.zhou@gmail.com>
2021-05-13 11:02:52 +0800

    ..

--- glTFRevitExport/Command.cs
+++ glTFRevitExport/Command.cs
@@ -1,5 +1,4 @@
 ﻿using System.IO;
-using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
@@ -8,26 +7,28 @@ using Microsoft.Win32;
 namespace glTFRevitExport
 {
     [Transaction(TransactionMode.Manual)]
-    class Command : IExternalCommand
+    public class Command : IExternalCommand
     {
-        public void ExportView3D(View3D view3d, string filename, string directory)
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            Document doc = view3d.Document;
-
-            // Use our custom implementation of IExportContext as the exporter context.
-            glTFExportContext ctx = new glTFExportContext(doc, filename, directory);
-            // Create a new custom exporter with the context.
-            CustomExporter exporter = new CustomExporter(doc, ctx);
-
-            exporter.ShouldStopOnError = true;
-            exporter.Export(view3d);
+            return Execute(commandData);
         }
 
-        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+       public Result Execute(UIApplication uiapp)
+        {
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            return Execute(uidoc);
+        }
+ 
+        public Result Execute(ExternalCommandData commandData)
         {
             UIApplication uiapp = commandData.Application;
             UIDocument uidoc = uiapp.ActiveUIDocument;
-            Application app = uiapp.Application;
+            return Execute(uidoc);
+        }
+
+        public Result Execute(UIDocument uidoc)
+        {
             Document doc = uidoc.Document;
 
             View3D view = doc.ActiveView as View3D;
@@ -52,5 +53,19 @@ namespace glTFRevitExport
 
             return Result.Succeeded;
         }
+
+        public void ExportView3D(View3D view3d, string filename, string directory)
+        {
+            Document doc = view3d.Document;
+
+            // Use our custom implementation of IExportContext as the exporter context.
+            glTFExportContext ctx = new glTFExportContext(doc, filename, directory);
+            // Create a new custom exporter with the context.
+            CustomExporter exporter = new CustomExporter(doc, ctx);
+
+            exporter.ShouldStopOnError = true;
+            exporter.Export(view3d);
+        }
+ 
     }
 }
